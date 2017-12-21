db890a5ee03d31d6a522918de4f8fdb539ee8b63
leeuwk004 <lion.drie@gmail.com>
2017-12-21 13:06:45 +0100

    audio boooooiii

---
+++ MerryChristmas/Assets/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        audio.loop = true;
+        audio.volume = 0.05f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
